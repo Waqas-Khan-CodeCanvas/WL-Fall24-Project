925449a0298ba0843d5094afda83b52f25bad33b
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-01-19 07:37:29 +0500

    routes and api end points

--- backend/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const controller = require("../controllers/user.controller");
-const auth = require("../middlewares/auth.middleware");
-
-// USER → only their own profile
-router.get("/me", auth, controller.getMyProfile);
-
-module.exports = router;
